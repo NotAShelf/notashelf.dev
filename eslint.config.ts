508936f02b72d9d633599cc423de386d1a1b76f5
NotAShelf <raf@notashelf.dev>
2025-04-12 18:28:48 +0300

    meta: convert eslint config to TS; fix errors

---
+++ eslint.config.ts
@@ -0,0 +1,46 @@
+import eslint from "@eslint/js";
+import eslintPluginAstro from "eslint-plugin-astro";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+const config = tseslint.config(
+  // Astro
+  ...eslintPluginAstro.configs.recommended,
+
+  // JavaScript
+  eslint.configs.recommended,
+  // TypeScript
+  ...tseslint.configs.recommended,
+
+  {
+    // Global config
+    ignores: [
+      "**/dist",
+      "**/node_modules",
+      "**/.astro",
+      "**/.github",
+      "**/result",
+    ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+
+  {
+    // Allow triple-slash references in `*.d.ts` files.
+    files: ["**/*.d.ts"],
+    rules: {
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
+
+  {
+    // Set globals for Node scripts.
+    files: ["src/scripts/**"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+);
+
+export default config;
