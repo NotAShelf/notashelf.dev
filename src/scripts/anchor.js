2c5892cb907d1538ade7f1767a86a6072ee4366f
NotAShelf <raf@notashelf.dev>
2025-04-14 21:13:00 +0300

    treewide: fmt

--- src/scripts/anchor.js
+++ src/scripts/anchor.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Smooth scroll for anchor links
-    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-      anchor.addEventListener("click", (e) => {
-        e.preventDefault();
-        const targetId = anchor.getAttribute("href");
-        const targetElement = document.querySelector(targetId);
+  // Smooth scroll for anchor links
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetId = anchor.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
 
-        if (targetElement) {
-          targetElement.scrollIntoView({
-            behavior: "smooth",
-          });
-        }
-      });
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
     });
   });
+});
