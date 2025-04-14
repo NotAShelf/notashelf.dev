5dc4d4d21d58246925ce88bd00c45372a3842f6f
NotAShelf <raf@notashelf.dev>
2025-04-15 01:56:07 +0300

    content: clean up config; add project and tag data collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
-import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+import { defineCollection, z, reference } from "astro:content";
+import { file, glob } from "astro/loaders";
 
-const postsCollection = defineCollection({
+const posts = defineCollection({
   loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./posts" }),
   schema: z.object({
     title: z.string().max(99),
@@ -14,6 +14,28 @@ const postsCollection = defineCollection({
   }),
 });
 
+const tags = defineCollection({
+  loader: file(`./src/data/tags.json`),
+  schema: z.object({
+    id: z.string(),
+  }),
+});
+
+const projects = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.json", base: `./src/data/projects` }),
+  schema: () =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      sourceUrl: z.string(),
+      startDate: z.coerce.date().optional(),
+      tags: z.array(reference("tags")).default([]),
+      active: z.boolean().default(true),
+    }),
+});
+
 export const collections = {
-  posts: postsCollection,
+  posts,
+  tags,
+  projects,
 };
