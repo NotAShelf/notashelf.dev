8e4f494e7f612a0d00d89513a6d49c0b3c52e00a
NotAShelf <raf@notashelf.dev>
2025-04-14 16:06:50 +0300

    content: limit title string; coerce date into zod date type & add 'updated'

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,10 @@ import { defineCollection, z } from "astro:content";
 const postsCollection = defineCollection({
   type: "content",
   schema: z.object({
-    title: z.string(),
+    title: z.string().max(99),
     description: z.string().optional(),
-    date: z.string(),
+    date: z.coerce.date(),
+    updated: z.date().optional(),
     keywords: z.array(z.string()).default([]),
     draft: z.boolean().default(false),
     archived: z.boolean().optional().default(false),
