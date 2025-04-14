37df2136f51a9f7f65751c8e900d61f270630bc7
NotAShelf <raf@notashelf.dev>
2025-04-15 01:56:31 +0300

    lib: add custom types for new data collections; re-export in lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
+// Remark plugins
 export { default as remarkEmDash } from "./plugins/remarkEmDash";
+
+// Export my custom types
+import * as types from "./types";
+export { types };
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,7 @@
 import type { CollectionEntry } from "astro:content";
 
+// Define explicit types for collections
 export type PostEntry = CollectionEntry<"posts">;
 export type ArchiveEntry = CollectionEntry<"posts">;
+export type TagEntry = CollectionEntry<"tags">;
+export type ProjectEntry = CollectionEntry<"projects">;
