f6eded4e944f5a5e07392edf799022e9653f4102
NotAShelf <raf@notashelf.dev>
2025-04-18 01:13:45 +0300

    pages/robots.txt: block more crawlers because I hate them

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,13 +1,12 @@
 export async function GET(): Promise<Response> {
-  const resp = new Response(
-    `
+  const content = `
 User-agent: PerplexityBot
 Disallow: /
 
 User-agent: FacebookBot
 Disallow: /
 
-User-agent:  OmigiliBot
+User-agent: OmigiliBot
 Disallow: /
 
 User-agent: anthropic-ai
@@ -19,9 +18,6 @@ Disallow: /
 User-agent: ChatGPT-User
 Disallow: /
 
-User-agent: PerplexityBot
-Disallow: /
-
 User-agent: CCBot
 Disallow: /
 
@@ -31,14 +27,55 @@ Disallow: /
 User-agent: GoogleOther
 Disallow: /
 
+User-agent: ClaudeBot
+Disallow: /
+
+User-agent: Bytespider
+Disallow: /
+
+User-agent: Sogou
+Disallow: /
+
+User-agent: YisouSpider
+Disallow: /
+
+User-agent: PetalBot
+Disallow: /
+
+User-agent: AhrefsBot
+Disallow: /
+
+User-agent: SemrushBot
+Disallow: /
+
+User-agent: MJ12bot
+Disallow: /
+
+User-agent: BLEXBot
+Disallow: /
+
+User-agent: DotBot
+Disallow: /
+
+User-agent: Baiduspider
+Disallow: /
+
+User-agent: SeekportBot
+Disallow: /
+
+User-agent: Exabot
+Disallow: /
+
+User-agent: Mail.RU_Bot
+Disallow: /
+
 User-agent: *
 Disallow:
-Allow: /
 
-Sitemap: ${import.meta.env.SITE}/sitemap.xml`.trim(),
-  );
+Sitemap: ${import.meta.env.SITE}/sitemap.xml
+`.trim();
 
+  const resp = new Response(content);
   resp.headers.set("Content-Type", "text/plain; charset=utf-8");
-
   return resp;
 }
