5206e3e0f7eab3a575c5cfc2a747b4b63939f472
NotAShelf <raf@notashelf.dev>
2025-06-07 21:01:34 +0300

    meta: begin setting up stryker typescript checker

--- stryker.config.mjs
+++ stryker.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
-/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+/** @type {import('@stryker-mutator/core').PartialStrykerOptions} */
 const config = {
   mutate: ["apps/**/*.{ts,tsx}"], // TODO: more modules must be tested.
   packageManager: "pnpm",
   reporters: ["html", "clear-text", "progress"],
   testRunner: "vitest",
   coverageAnalysis: "perTest",
-  plugins: ["@stryker-mutator/vitest-runner"],
+  plugins: [
+    "@stryker-mutator/vitest-runner",
+    "@stryker-mutator/typescript-checker",
+  ],
   tempDirName: ".stryker-tmp",
   tsconfigFile: "tsconfig.json",
-  ignorePatterns: [".direnv/**", "**/*.nix"],
+  checker: ["typescript"],
+  typescriptChecker: {
+    prioritizePerformanceOverAccuracy: true,
+  },
+  ignorePatterns: [".direnv/**", "**/*.nix", "result*", "stryker-tmp/**"],
 };
 
 export default config;
