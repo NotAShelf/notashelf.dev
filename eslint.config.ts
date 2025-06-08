1bf06e0a7f586df50bd69d4df83d025063ab9cb5
NotAShelf <raf@notashelf.dev>
2025-06-08 13:51:12 +0300

    meta: update eslint config for scripts at repo root

--- eslint.config.ts
+++ eslint.config.ts
@@ -43,7 +43,7 @@ const config = tseslint.config(
 
   {
     // Set globals for Node scripts.
-    files: ["src/scripts/**"],
+    files: ["scripts/**", "src/scripts/**"],
     languageOptions: {
       globals: globals.node,
     },
