ad95def6213182abb07284b532bbc3c0327f8be9
NotAShelf <62766066+NotAShelf@users.noreply.github.com>
2025-08-15 10:58:03 +0000

    meta: auto-update dependencies

--- nix/site.nix
+++ nix/site.nix
@@ -90,7 +90,7 @@ in
 
     pnpmDeps = pnpm.fetchDeps {
       inherit (finalAttrs) pname src;
-      hash = "sha256-QxUWcs4gdY2CWpWYsSW7O1sudW9MAxb517u/tYoQz2c=";
+      hash = "sha256-/3lB2822KaETVYLaWK5IOdRXelFBP4/TJUeaaXzp8TE=";
       fetcherVersion = 2; # https://nixos.org/manual/nixpkgs/stable/#javascript-pnpm-fetcherVersion
     };
 
