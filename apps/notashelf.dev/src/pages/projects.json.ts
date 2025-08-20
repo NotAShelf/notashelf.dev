d64eff858eddd63a0c8d273e134640a734046ff3
NotAShelf <raf@notashelf.dev>
2025-08-20 15:31:52 +0300

    notashelf.dev: make project list deterministic
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a6964d2b9d872268568bcd1202cb7698771db

--- apps/notashelf.dev/src/pages/projects.json.ts
+++ apps/notashelf.dev/src/pages/projects.json.ts
@@ -1,17 +1,27 @@
 import { getCollection } from "astro:content";
+import type { APIRoute } from "astro";
+import type { ProjectEntry } from "@lib/types";
 
-export async function GET() {
+export const GET: APIRoute = async () => {
   const allProjects = await getCollection("projects");
-  // Emit all fields for each project entry
-  const projects = allProjects.map((entry) => ({
-    ...entry.data,
-    id: entry.id,
-    slug: entry.slug,
-    body: entry.body,
-    link: entry.data.sourceUrl,
-  }));
+  // Emit all fields for each project entry.
+  // This is sorted **by ID** for added determinism.
+  const projects = allProjects
+    .map((entry: ProjectEntry) => ({
+      ...entry.data,
+      id: entry.id,
+      slug: entry.slug,
+      body: entry.body,
+      link: entry.data.sourceUrl,
+    }))
+    .sort(
+      (
+        a: ProjectEntry["data"] & { id: string },
+        b: ProjectEntry["data"] & { id: string },
+      ) => a.id.localeCompare(b.id),
+    );
 
   return new Response(JSON.stringify(projects, null, 2), {
     headers: { "Content-Type": "application/json" },
   });
-}
+};
