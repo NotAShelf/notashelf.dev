5ef6197e83f625b7f224a5042b3d0b8443b0df0a
NotAShelf <62766066+NotAShelf@users.noreply.github.com>
2026-05-17 10:48:41 +0000

    meta: auto-update dependencies

--- nix/site.nix
+++ nix/site.nix
@@ -52,7 +52,7 @@ in
     # to fetch deps for and build. Alas, NodeJS.
     pnpmDeps = fetchPnpmDeps {
       inherit (finalAttrs) pname src pnpmInstallFlags;
-      hash = "sha256-Cs8e4AoWpHHESlinyW4xSezwSwwIKcjJUO+PxM9Yd6w=";
+      hash = "sha256-DwUCOGz5FO58BWNDZ9PM5b84FNwXRy7/YuADyX8eI8s=";
       fetcherVersion = 3; # https://nixos.org/manual/nixpkgs/stable/#javascript-pnpm-fetcherVersion
     };
 
